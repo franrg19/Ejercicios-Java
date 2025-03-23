ec70d25a54f76e8f822bf5bb23e1770c19830945
franrg19 <franrg19@outlook.com>
2025-03-23 21:45:38 +0100

    Fin del ejercicio calculadora

--- src/EjerciciosClase5/EjerciciosClase5.java
+++ src/EjerciciosClase5/EjerciciosClase5.java
@@ -37,15 +37,44 @@ public class EjerciciosClase5 {
         sc.close();
 
 
+        Scanner scn = new Scanner(System.in);
 
+        System.out.println("Menú:");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
+        System.out.print("Elige una opción: ");
 
+        int opcion = scn.nextInt();
 
+        System.out.print("Ingresa el primer número: ");
+        double num1 = scn.nextDouble();
+        System.out.print("Ingresa el segundo número: ");
+        double num2 = scn.nextDouble();
 
+        switch (opcion) {
+            case 1:
+                System.out.println("Resultado: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Resultado: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Resultado: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Resultado: " + (num1 / num2));
+                } else {
+                    System.out.println("No se puede dividir por cero.");
+                }
+                break;
+            default:
+                System.out.println("Opción no válida.");
+        }
 
-
-
-
-
+        scanner.close();
 
 
     }
